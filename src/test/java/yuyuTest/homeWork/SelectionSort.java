43ad5208b75c398e22370ac868a5b47a8f530602
yuyusgithub <342476212@qq.com>
2017-11-13 14:41:11 +0800

    test

--- src/test/java/yuyuTest/homeWork/SelectionSort.java
+++ src/test/java/yuyuTest/homeWork/SelectionSort.java
@@ -5,65 +5,9 @@ package yuyuTest.homeWork;
  */
 public class SelectionSort {
 
-
-    private static void sort5(Comparable[] array) {
-        int size = array.length;
-        for (int i = 0; i < size; i++) {
-            int minIndex = i;
-            for(int j = i+1;j<size;j++){
-                if (array[j].compareTo(array[minIndex]) < 0){
-                    minIndex = j;
-                }
-            }
-            Utils.swap(array,minIndex,i);
-        }
-    }
-
-
-    private static void sort4(Comparable[] array) {
-        int size = array.length;
-        for (int i = 0; i < size; i++) {
-            int minIndex = i;
-            for (int j = i + 1; j < size; j++) {
-                if (array[j].compareTo(array[minIndex]) < 0) {
-                    minIndex = j;
-                }
-            }
-            Utils.swap(array, i, minIndex);
-        }
-    }
-
-    private static void sort3(Comparable[] array) {
-        int size = array.length;
-        for (int i = 0; i < size; i++) {
-            int minIndex = i;
-            for (int j = i + 1; j < size; j++) {
-                if (array[j].compareTo(array[minIndex]) < 0) {
-                    minIndex = j;
-                }
-            }
-            Utils.swap(array, i, minIndex);
-        }
-    }
-
-
-    private static void sort2(Comparable[] array) {
-        int size = array.length;
-        for (int i = 0; i < size; i++) {
-            int minIndex = i;
-            for (int j = i + 1; j < size; j++) {
-                if (array[i].compareTo(array[j]) > 0) {
-                    minIndex = j;
-                }
-            }
-            Utils.swap(array, minIndex, i);
-        }
-    }
-
-
     public static void main(String[] args) {
         Integer[] datas = Utils.random(10, 1, 100);
-        sort5(datas);
+        sort(datas);
         Utils.printt(datas);
     }
 
@@ -80,17 +24,5 @@ public class SelectionSort {
         }
     }
 
-    private static void sort1(Comparable[] array) {
-        int size = array.length;
-        for (int i = 0; i < size; i++) {
-            int mixIndex = i;
-            int j = i + 1;
-            for (; j < size; j++) {
-                if (array[mixIndex].compareTo(array[j]) > 0) {
-                    mixIndex = j;
-                }
-            }
-            Utils.swap(array, mixIndex, i);
-        }
-    }
+
 }
